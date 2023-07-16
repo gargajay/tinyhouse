0824a890c389dbac2c7129fa8a833f43c1e3255a
Ajay garg <ajay.garg579@gmail.com>
2023-07-16 09:29:44 +0530

    update public

--- resources/views/layouts/admin/admin.blade.php
+++ resources/views/layouts/admin/admin.blade.php
@@ -10,13 +10,13 @@
 
 	<!-- Global stylesheets -->
 	<link href="https://fonts.googleapis.com/css?family=Roboto:400,300,100,500,700,900" rel="stylesheet" type="text/css">
-	<link href="{{ asset('admin') }}/css/icons/icomoon/styles.min.css" rel="stylesheet" type="text/css">
-	<link href="{{ asset('admin') }}/css/icons/fontawesome/styles.min.css" rel="stylesheet" type="text/css">
-	<link href="{{ asset('admin') }}/css/all.min.css" rel="stylesheet" type="text/css">
+	<link href="{{ asset('public/admin') }}/css/icons/icomoon/styles.min.css" rel="stylesheet" type="text/css">
+	<link href="{{ asset('public/admin') }}/css/icons/fontawesome/styles.min.css" rel="stylesheet" type="text/css">
+	<link href="{{ asset('public/admin') }}/css/all.min.css" rel="stylesheet" type="text/css">
 
 	{{-- <link href="https://cdn.datatables.net/1.11.3/css/jquery.dataTables.min.css" rel="stylesheet" type="text/css"> --}}
 
-	<link href="{{ asset('admin') }}/css/common.css" rel="stylesheet" type="text/css">
+	<link href="{{ asset('public/admin') }}/css/common.css" rel="stylesheet" type="text/css">
 
 
 	@yield('page_style')
@@ -38,32 +38,32 @@
 	<!-- /page content -->
 
 	<!-- Core JS files -->
-	<script src="{{ asset('admin') }}/js/main/jquery.min.js"></script>
-	<script src="{{ asset('admin') }}/js/main/bootstrap.bundle.min.js"></script>
+	<script src="{{ asset('public/admin') }}/js/main/jquery.min.js"></script>
+	<script src="{{ asset('public/admin') }}/js/main/bootstrap.bundle.min.js"></script>
 	<!-- /core JS files -->
 
 	<!-- Theme JS files -->
-	<script src="{{ asset('admin') }}/js/plugins/visualization/d3/d3.min.js"></script>
-	<script src="{{ asset('admin') }}/js/plugins/visualization/d3/d3_tooltip.js"></script>
-	<script src="{{ asset('admin') }}/js/plugins/ui/moment/moment.min.js"></script>
-	<script src="{{ asset('admin') }}/js/plugins/pickers/daterangepicker.js"></script>
+	<script src="{{ asset('public/admin') }}/js/plugins/visualization/d3/d3.min.js"></script>
+	<script src="{{ asset('public/admin') }}/js/plugins/visualization/d3/d3_tooltip.js"></script>
+	<script src="{{ asset('public/admin') }}/js/plugins/ui/moment/moment.min.js"></script>
+	<script src="{{ asset('public/admin') }}/js/plugins/pickers/daterangepicker.js"></script>
 	<script src="https://cdn.jsdelivr.net/npm/chart.js"></script>
 
 	{{-- <script src="https://cdn.datatables.net/1.11.3/js/jquery.dataTables.min.js"></script> --}}
 	<!-- Theme JS files -->
-    <script src="{{ asset('admin') }}/js/plugins/tables/datatables/datatables.min.js"></script>
+    <script src="{{ asset('public/admin') }}/js/plugins/tables/datatables/datatables.min.js"></script>
 
-    <script src="{{ asset('admin') }}/js/demo_pages/datatables_basic.js"></script>
+    <script src="{{ asset('public/admin') }}/js/demo_pages/datatables_basic.js"></script>
 
-	<script src="{{ asset('admin') }}/js/demo_pages/dashboard.js"></script>
-	<script src="{{ asset('admin') }}/js/jquery.validate.min.js"></script>
-	<script src="{{ asset('admin') }}/js/jquery.shorten.min.js"></script>
-	<script src="{{ asset('admin') }}/js/plugins/notifications/sweet_alert.min.js"></script>
+	<script src="{{ asset('public/admin') }}/js/demo_pages/dashboard.js"></script>
+	<script src="{{ asset('public/admin') }}/js/jquery.validate.min.js"></script>
+	<script src="{{ asset('public/admin') }}/js/jquery.shorten.min.js"></script>
+	<script src="{{ asset('public/admin') }}/js/plugins/notifications/sweet_alert.min.js"></script>
 
 
-	<script src="{{ asset('admin') }}/js/app.js"></script>
-	<script src="{{ asset('admin') }}/js/demo_pages/extra_sweetalert.js"></script>
-	<script src="{{ asset('admin') }}/js/common.js"></script>
+	<script src="{{ asset('public/admin') }}/js/app.js"></script>
+	<script src="{{ asset('public/admin') }}/js/demo_pages/extra_sweetalert.js"></script>
+	<script src="{{ asset('public/admin') }}/js/common.js"></script>
 
 	@yield('page_script')
 </body>
--- resources/views/layouts/session.blade.php
+++ resources/views/layouts/session.blade.php
@@ -9,9 +9,9 @@
     <link rel="icon" type="image/png" sizes="16x16" href="{{ asset('images/logo.png') }}">
 
     <link href="https://fonts.googleapis.com/css?family=Roboto:400,300,100,500,700,900" rel="stylesheet" type="text/css">
-    <link href="{{ asset('admin') }}/css/icons/icomoon/styles.min.css" rel="stylesheet" type="text/css">
-    <link href="{{ asset('admin') }}/css/all.min.css" rel="stylesheet" type="text/css">
-    <link href="{{ asset('admin') }}/css/session-page.css" rel="stylesheet" type="text/css">
+    <link href="{{ asset('public/admin') }}/css/icons/icomoon/styles.min.css" rel="stylesheet" type="text/css">
+    <link href="{{ asset('public/admin') }}/css/all.min.css" rel="stylesheet" type="text/css">
+    <link href="{{ asset('public/admin') }}/css/session-page.css" rel="stylesheet" type="text/css">
 
     <style>
         .login-cover {
@@ -32,11 +32,11 @@
 
     </div>
 
-    <script src="{{ asset('admin') }}/js/main/jquery.min.js"></script>
-    <script src="{{ asset('admin') }}/js/main/bootstrap.bundle.min.js"></script>
-    <script src="{{ asset('admin') }}/js/app.js"></script>
-    <script src="{{ asset('admin') }}/js/jquery.validate.min.js"></script>
-    <script src="{{ asset('admin') }}/js/jquery.shorten.min.js"></script>
+    <script src="{{ asset('public/admin') }}/js/main/jquery.min.js"></script>
+    <script src="{{ asset('public/admin') }}/js/main/bootstrap.bundle.min.js"></script>
+    <script src="{{ asset('public/admin') }}/js/app.js"></script>
+    <script src="{{ asset('public/admin') }}/js/jquery.validate.min.js"></script>
+    <script src="{{ asset('public/admin') }}/js/jquery.shorten.min.js"></script>
     @yield('page_script')
 
     <script>
